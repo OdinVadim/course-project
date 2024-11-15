0d8a950e50c1b7bf6d529966ec55b8b1873f015d
Alphxuy <kuzmenko.matvey06@gmail.com>
2024-11-16 01:22:45 +0300

    add all sorting methods

--- server/src/algorithm.cpp
+++ server/src/algorithm.cpp
@@ -8,7 +8,7 @@ void selectionSort(vector<int>& arr)
     for (int i = 0;i < n - 1; i++)
     {
         int min_idx = 1;
-        for (int j = i+1; j>n; j++)
+        for (int j = i+1; j < n; j++)
         {
             if (arr[j] < arr[min_idx])
             {
@@ -21,4 +21,24 @@ void selectionSort(vector<int>& arr)
         }
     }
 }
-
+void shellSort(vector<int>& arr)
+{
+    int n = arr.size();
+    int gap = n / 2;
+    while(gap > 0)
+    {
+        for (int i = gap; i < n; i++)
+        {
+            int temp = arr[i];
+            int j = i;
+            while (j >= gap && arr[j - gap] > temp)
+            {
+                arr[j] = arr[j-gap];
+                j -= gap;
+            }
+            arr[j] = temp;
+            
+        }
+        gap /= 2;
+    }
+}
