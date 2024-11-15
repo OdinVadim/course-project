6b2b2755b4980101e846d70ad082e77283cb4caa
Alphxuy <kuzmenko.matvey06@gmail.com>
2024-11-16 01:22:45 +0300

    add all sorting methods

---
+++ server/include/sorts.h
@@ -0,0 +1,8 @@
+#ifndef sorts_h
+#define sorts_h
+
+#include <vector>
+using namespace std;
+void selectionSort(vector<int>& arr);
+void shellSort(vector<int>& arr);
+#endif //sorts_h
--- server/src/server.cpp
+++ server/src/server.cpp
@@ -1,23 +1,13 @@
 #include <iostream>
 #include <vector>
 #include "h.h"
-#include "select.h"
+#include "sorts.h"
+
 
 using namespace std;
 
 int main()
 {
     cout << "It's server " << kafedra << "\n";
-    vector<int> arr = {64, 25, 12, 22, 11};
-
-    selectionSort(arr);
-
-    cout <<"Sorted arr: ";
-    for (int i = 0; i < arr.size(); i++)
-    {
-        cout << arr[i] << " ";
-    }
-    cout << endl;
-
     return 0;
 }
