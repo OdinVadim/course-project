7330f4eefd55576ead6ab26b6db5cd2cc1a3f9f0
orange223171 <msyromyatnikov.06@gmail.com>
2024-11-19 20:47:07 +0300

    Add int create_socket() for client

--- client/include/connection.h
+++ client/include/connection.h
@@ -1,1 +1,22 @@
-#pragma once
+#ifndef CLIENT_CONNECTION_H
+#define CLIENT_CONNECTION_H
+
+#include <string>
+
+//Подключение к серверу
+int connect_to_server();
+//Отключение от сервера
+void disconnect_from_server(int socket);
+
+//Обработка подключения
+int handle_connection(int socket);
+
+//Отправка сообщение серверу
+int send_message(const std::string& message);
+//Приём сообщения от сервера
+int recieve_message(std::string& message);
+
+//Создание сокета
+int create_socket();
+
+#endif //CLIENT_CONNECTION_H
--- client/src/connection.cpp
+++ client/src/connection.cpp
@@ -2,5 +2,90 @@
 
 //Подключаем заголовочный файл, который предоставляет реализацию сокетов на Unix-системах
 #ifdef UNIX
-#include <sys/socket.h>
+
+#include <sys/socket.h> 
+#include <netdb.h>
+
+#include <unistd.h>
+
+#include <iostream>
+//Файл, содержащий функцию memset()
+#include <cstring>
+
+//IP-адрес и номер порта
+const char* ip_address = "127.0.0.1";
+const char* port = "8080";
+
+int connect_to_server()
+{
+    return 0;
+}
+void disconnect_from_server(int socket)
+{
+    return;
+}
+
+int handle_connection(int socket)
+{
+    return 0;
+}
+
+int send_message(const std::string& message)
+{
+    return 0;
+}
+int recieve_message(std::string& message)
+{
+    return 0;
+}
+
+int create_socket()
+{
+    //addrinfo хранит в себе информацию об адресе, по которому будет происходить подключение
+    //В hints записываются данные о типе подключения, которые потом будут перезаписаны в address
+    //adress будет использоваться для записи итогового адреса
+    addrinfo hints, *address;
+    
+    //Очищаем переменную hints c помощью функции memset, чтобы в adress не попало никаких лишних данных
+    std::memset(&hints, 0x00, sizeof(addrinfo));
+    //Используем IPv4
+    hints.ai_family = AF_INET;
+    //Используем тип сокета TCP
+    hints.ai_socktype = SOCK_STREAM;
+    //Используем протокол TCP
+    hints.ai_protocol = IPPROTO_TCP;
+
+    //Записываем адрес по указателю adress, если функция возвращает не ноль, значит произошла ошибка    
+    if (getaddrinfo(ip_address, port, &hints, &address) != 0)
+    {
+        std::cout << "[Error] Failed to resolve address\n";
+        return -1;
+    }
+
+    //Создаём сокет
+    int client_socket = socket(address->ai_family, address->ai_socktype, address->ai_protocol);
+
+    //Проверяем созданный сокет
+    if (client_socket < 0)
+    {
+        std::cout << "[Error] Failed to create server_socket\n";
+
+        //Освобождаем память от address
+        freeaddrinfo(address);
+        return -1;
+    }
+
+    //Подключаемся к серверу по адресу
+    if (connect(client_socket, address->ai_addr, address->ai_addrlen) == -1)
+    {
+        std::cout << "[Error] Failed to connect to server\n";
+        return -1;
+    }
+
+    //Освобождаем память от address
+    freeaddrinfo(address);
+
+    return client_socket;
+}
+
 #endif /*UNIX*/
