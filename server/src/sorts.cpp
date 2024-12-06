b3dff6fc3915f7dad8b861332ef50ab348a39418
orange223171 <msyromyatnikov.06@gmail.com>
2024-12-07 00:08:00 +0300

    Fix selection sort (again)

--- server/src/sorts.cpp
+++ server/src/sorts.cpp
@@ -10,7 +10,7 @@ void selection_sort(vector<int>& arr)
     int n = arr.size();
     for (int i = 0;i < n - 1; i++)
     {
-        int min_idx = 1;
+        int min_idx = i;
         for (int j = i+1; j < n; j++)
         {
             if (arr[j] < arr[min_idx])
