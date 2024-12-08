1656e6d6184ea231cc7c9c668f5a5a1c8b435ea5
orange223171 <msyromyatnikov.06@gmail.com>
2024-12-08 12:37:38 +0300

    Add command search

--- client/src/command.cpp
+++ client/src/command.cpp
@@ -76,6 +76,127 @@ int command_echo(int socket)
     return 0;
 }
 
+int command_search(int socket)
+{
+    //Ввод метода поиска
+    std::string method;
+    std::cin >> method;
+
+    //Ввод ключа поиска key
+    int key;
+    std::cin >> key;
+
+    //Ввод пути файла
+    std::string input_file_name;
+    std::cin >> input_file_name;
+
+    //Открываем файл
+    std::ifstream input_file;
+    input_file.open(input_file_name);
+
+    //Проверяем, открылся ли файл
+    if(!input_file.is_open())
+    {
+        std::cout << "[Error] Failed to open file " << input_file_name << "\n";
+        return 0;
+    }
+
+    //Размер файла
+    unsigned int file_size = 0;
+
+    //Вычисляем размер файла
+    while(!input_file.eof())
+    {
+        int b;
+        input_file >> b;
+
+        if (input_file.fail())
+        {
+            std::cout << "[Error] File contains uncorrect data\n";
+            return 0;
+        }
+        file_size++;
+    }
+
+    //Возвращаемся к началу файла
+    input_file.clear();
+    input_file.seekg(0, std::ios::beg);
+
+    //Создаём сообщение message, которое будет передано на сервер
+    std::vector<char> message = std::vector<char>(file_size*int_size + 5);
+
+    //Записываем ключ поиска key в message
+    message[1] = (key & 0xFF00'0000) >> 24;
+    message[2] = (key & 0x00FF'0000) >> 16;
+    message[3] = (key & 0x0000'FF00) >> 8;
+    message[4] = (key & 0x0000'00FF);
+    
+    //Записываем массив чисел в message
+    for (int i = 0; i < file_size; i++)
+    {
+        int b;
+        input_file >> b;
+
+        message[i*int_size + 5] = (b & 0xFF00'0000) >> 24;
+        message[i*int_size + 6] = (b & 0x00FF'0000) >> 16;
+        message[i*int_size + 7] = (b & 0x0000'FF00) >> 8;
+        message[i*int_size + 8] = (b & 0x0000'00FF);
+    }
+
+    //Закрываем файл
+    input_file.close();
+
+    //Линейный поиск
+    if (method == "linear")
+    {
+        message[0] = char(server_command::linear_search);
+    }
+    //Линейный поиск с барьером
+    else if (method == "sentinel-linear")
+    {
+        message[0] = char(server_command::sentinel_linear_search);
+    }
+    //Бинарный поиск
+    else if (method == "binary")
+    {
+        message[0] = char(server_command::binary_search);
+    }
+    //Линейный поиск с сортровкой
+    else if (method == "sort-linear")
+    {
+        message[0] = char(server_command::linear_search_with_sort);
+    }
+    //Неизвестный метод поиска
+    else
+    {
+        std::cout << "[Info] " << method << " is unknown method\n";
+        return 0;
+    }
+
+    //Отправка сообщения на сервер
+    if (send_message(socket, message) < 0)
+    {
+        return -1;
+    }
+
+    //Приём сообщения от сервера
+    if (recieve_message(socket, message) < 0)
+    {
+        return -1;
+    }
+
+    unsigned char a1 = (message[0]);
+    unsigned char a2 = (message[1]);
+    unsigned char a3 = (message[2]);
+    unsigned char a4 = (message[3]);
+        
+    int b = (a1 << 24) | (a2 << 16) | (a3 << 8) | a4;
+
+    std::cout << "[Server] Key of searching element is " << b << "\n";
+
+    return 0;
+}
+
 int command_sort(int socket)
 {
     //Ввод метода сортировки
@@ -226,6 +347,11 @@ int handle_command(int socket, const std::string& command)
     else if (command == "echo")
     {
         return command_echo(socket);
+    }
+    //Поиск
+    else if (command == "search")
+    {
+        return command_search(socket);
     }
     //Сортировка
     else if (command == "sort")
